49b10c654b83dd339803e121c6fb5e41e1f53fa2
JanekitPk <6210110051@psu.ac.th>
2021-07-05 11:27:24 +0700

    t

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,9 +1,24 @@
 import CharacterCard from './CharacterCard';
 function App() {
  return (
+ <div className="App">
  <div>
  <CharacterCard value="h"/>
  <CharacterCard value="i"/>
+ </div>
+ <p>
+ Hello {"World"}
+ </p>
+ </div>
+ );
+}
+const word = "Hello";
+function App() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
  </div>
  );
 }
