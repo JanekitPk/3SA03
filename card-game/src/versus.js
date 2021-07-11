d6b5d3f25083ff901c56d6c1bacf2d2620f5cc0c
JanekitPk <6210110051@psu.ac.th>
2021-07-11 14:48:07 +0700

    Part7-2

--- card-game/src/versus.js
+++ card-game/src/versus.js
@@ -3,11 +3,13 @@ const choose = ['rock','scissors','paper']
 state = {
     P1:choose[0],P2:choose[0]
   };
-const versus = (props) => {
+export default function versus(props){  
     this.setState({ 
         P1:choose[props],
         P2:choose[Math.floor(Math.random()*choose.length)]
     })
+
+
     return(
         <div className='App'>
 
